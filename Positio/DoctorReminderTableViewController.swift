cd52da60edc0ef86f8fd6dfa0fa7b19ecf9ad997
Bhagat Singh <bhagatsingh2297@gmail.com>
2017-02-11 16:31:32 +0530

    Final Commit

--- Positio/DoctorReminderTableViewController.swift
+++ Positio/DoctorReminderTableViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class DoctorReminderTableViewController: UITableViewController {
 
+    var names = ["Dr. Rishi", "Dr. Majumdar"]
+    var address = ["Kolmet Hospital", "Sri Ganga Ram"]
+    var images = ["heart","lungs"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -29,23 +34,23 @@ class DoctorReminderTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return names.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "doctorCell", for: indexPath) as! DoctorReminderTableViewCell
+        
         // Configure the cell...
 
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
